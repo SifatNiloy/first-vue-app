6ec80c92bfc50ddad4d7990e4f7fbdbbf713cf7d
unknown <sifatniloy18@gmail.com>
2022-12-29 11:22:39 -0600

    styles fixed and showed home component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Home from './components/Home.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
+      <Home data="sifat is here" msg="we are brothers"></Home>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    
   </main>
 </template>
 
 <style scoped>
+
+
 header {
   line-height: 1.5;
 }
@@ -44,4 +48,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> 
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Hello from home component</h1>
+        <h1>{{ data }}</h1>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+<script>
+export default{
+    name: 'Home',
+    props:{
+        data: String,
+        msg: String
+    }
+}
+</script>
