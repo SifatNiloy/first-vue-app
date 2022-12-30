097ab06b2a64ec87f61257571e06a8e1e3ea0709
SifatNiloy <sifatniloy18@gmail.com>
2022-12-30 07:42:57 -0800

    js using in vue

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="new">
         <h1>This is a new component</h1>
+        <h2>created by {{ name }}</h2>
+        <button v-on:click="hello">click here</button>
         <Users></Users>
         <Products></Products>
     </div>
@@ -8,16 +10,27 @@
 <script>
 import Users from './Users.vue';
 import Products from './Products.vue';
-export default{
+export default {
     name: "Home",
-    components:{
-    Users,
-    Products
-}
+    components: {
+        Users,
+        Products
+    },
+    data() {
+        return {
+            name: 'sifat'
+        }
+    },
+    methods: {
+        hello(){
+            alert('Hello clicked')
+        }
+    }
+
 }
 </script>
 <style>
-.new{
+.new {
     background-color: rgb(161, 208, 247);
     height: 800px;
     text-align: center;
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,18 +1,35 @@
 <template>
-    <div class="user"> 
+    <div class="user">
         <h1>Users component</h1>
+        {{ header }}
+        <p v-html="header"></p>
+        <p>{{ 1 + 100 }}</p>
+        <h3>{{ ok ? "yes" : "no" }}</h3>
+        <button v-bind:disabled="btnDisable" v-on:click="click">change value</button>
     </div>
 </template>
 <script>
-export default{
-    name: 'users'
+export default {
+    name: 'users',
+    data() {
+        return {
+            ok: true,
+            header:"<h1>template in vue js</h1>",
+            btnDisable: true
+        }
+    },
+    methods: {
+        click() {
+            this.ok= !this.ok
+        }
+    }
 }
 </script>
 <style>
-.user{
+.user {
     height: 300px;
     width: 300px;
-    background-color: rgb(92, 170, 3);
+    background-color: rgb(210, 247, 168);
     padding: 20px;
 }
 </style>
